fedf3cb5f0218e30768bcbd233d9bb47a062af99
Grand Nabil <akambilawani@gmail.com>
2023-08-18 12:27:03 +0100

    adding of route with @app.route

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, session
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def index():
+    return render_template('index.html')
